173dbf5365d037fc30a66e15e6ccc45058f50572
Miguel agudelo <agudelomiguel803-pixel>
2026-03-25 16:04:08 -0500

    Agrega getMovieById en api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,14 +1,18 @@
-import axios from "axios";
-
 const API_KEY = process.env.REACT_APP_TMDB_KEY;
 const BASE_URL = "https://api.themoviedb.org/3";
 
-export const getMovies = async (query) => {
-  const res = await axios.get(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${query}`);
-  return res.data.results;
-};
+export async function getMovies(query) {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&language=es-ES&query=${query}`
+  );
+  const data = await response.json();
+  return data.results;
+}
 
-export const getMovieDetails = async (id) => {
-  const res = await axios.get(`${BASE_URL}/movie/${id}?api_key=${API_KEY}`);
-  return res.data;
-};
+export async function getMovieById(id) {
+  const response = await fetch(
+    `${BASE_URL}/movie/${id}?api_key=${API_KEY}&language=es-ES`
+  );
+  const data = await response.json();
+  return data;
+}
